2b3af0c42730a689a0dfd058392b14884ae0ce92
Sam Don <71539578+sam-don@users.noreply.github.com>
2020-11-06 11:35:46 +1100

    split database into it's own file

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
-import psycopg2
-
-connection = psycopg2.connect(
-    database="library_api",
-    user="app",
-    password="Testing1",
-    host="localhost"
-)
-
-cursor = connection.cursor()
-
-cursor.execute("create table if not exists books (id serial PRIMARY KEY, title varchar);")
-connection.commit()
+from database import cursor, connection
 
 @app.route("/books", methods=["GET"])
 def book_index():
